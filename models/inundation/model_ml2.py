f0c4fbdff530501da3cfde48150d9cda49590f93
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-12-17 14:31:42 +0700

    update ml2

--- models/inundation/model_ml2.py
+++ models/inundation/model_ml2.py
@@ -73,8 +73,8 @@ def load_model_ml2(width, height, device):
     steps = 1
     model = CNNModelBN(steps=steps, features=features, outputs=output_size)
     model.to(device)
-    path_trained_model_ml2 = "./models/inundation/CNN Kabir 100 epoch 44 kasus Augmented.pth"
-    model.load_state_dict(torch.load(path_trained_model_ml2, map_location=torch.device('cpu')))
+    path_trained_model_ml2 = "./models/inundation/CNN Kabir Sukabumi best by ssim.pth"
+    model.load_state_dict(torch.load(path_trained_model_ml2,weights_only=True,map_location=torch.device('cpu')))
     model.eval()
     print("Successfully loaded ml2")
     return model
